a2ae584bda7513fdd3292ba0d88f40dff1ecc706
kivyswiftlink <kivyswiftlink@gmail.com>
2024-10-14 06:33:33 +0000

    Update from https://github.com/kivyswiftlink/CoreBuilder/commit/58eba3081b8ea0ea69301d10e7db23fd6726257a

--- Package.swift
+++ Package.swift
@@ -24,8 +24,8 @@ let package = Package(
     ], targets: [
         //.binaryTarget(name: "libpng16", path: "xcframework/libpng16.zip"),
         //.binaryTarget(name: "libjpeg", path: "xcframework/libjpeg.zip"),
-    	.binaryTarget(name: "libpng16", url: "https://github.com/KivySwiftLink/ImageCore/releases/download/311.0.1/libpng16.zip", checksum: "c4e7e7274dba058bac3fd421e47ddfb60c3731d9559109f37c88a50e79533d77"),
-    	.binaryTarget(name: "libjpeg", url: "https://github.com/KivySwiftLink/ImageCore/releases/download/311.0.1/libjpeg.zip", checksum: "5b8abc04c970165cdbf3f7c449ad65e19187af192277e5bafb94502b71ad73e0"),
+    	.binaryTarget(name: "libpng16", url: "https://github.com/KivySwiftLink/ImageCore/releases/download/311.0.0/libpng16.zip", checksum: "f7645c45413b538f16f5bc69bf63d53ec4517a367cda54612632d95526600326"),
+    	.binaryTarget(name: "libjpeg", url: "https://github.com/KivySwiftLink/ImageCore/releases/download/311.0.0/libjpeg.zip", checksum: "93d82d247c545f09def63057739c8ecef89cc9810a8d279cc3f3efa5c54c3013"),
     ]
 
 )
