ae3f48f3a1ca1302fd05dfa119edb482fe661ec6
kivyswiftlink <kivyswiftlink@gmail.com>
2024-12-19 00:32:06 +0000

    Update from https://github.com/kivyswiftlink/CoreBuilder/commit/3231d0717eab9c29acb8683e8e435adf35fbcc4a

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
-
-// swift-tools-version: 5.8
+// swift-tools-version: 5.9
 
 import PackageDescription
 
@@ -7,25 +6,21 @@ let package = Package(
     name: "ImageCore",
     platforms: [.iOS(.v13)],
     products: [
-    	.library(
-        	name: "libpng",
-        	targets: [
-        		"libpng16"
-        	]
-    	),
-    	.library(
-        	name: "libjpeg",
-        	targets: [
-        		"libjpeg"
-        	]
-    	),
+        .library(name: "libpng", targets: ["libpng16"]), .library(name: "libjpeg", targets: ["libjpeg"])
     ],
     dependencies: [
-    ], targets: [
-        //.binaryTarget(name: "libpng16", path: "xcframework/libpng16.zip"),
-        //.binaryTarget(name: "libjpeg", path: "xcframework/libjpeg.zip"),
-    	.binaryTarget(name: "libpng16", url: "https://github.com/KivySwiftLink/ImageCore/releases/download/311.0.4/libpng16.zip", checksum: "d2f340edec72ef590096e786c9a58a1f60dda9b7e2e88675e501041dc47f29dc"),
-    	.binaryTarget(name: "libjpeg", url: "https://github.com/KivySwiftLink/ImageCore/releases/download/311.0.4/libjpeg.zip", checksum: "a672ab7de4872e457085d53e857b4529e7e85744eee42268702fbd5ffbb28352"),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "libjpeg",
+            url: "https://github.com/kivyswiftlink/ImageCore/releases/download/311.0.5/libjpeg.zip",
+            checksum: "842fe12826f39728f46df2d58366292dd454ee899bc1d32c07eca828402eb6cb"
+        ),
+        .binaryTarget(
+            name: "libpng16",
+            url: "https://github.com/kivyswiftlink/ImageCore/releases/download/311.0.5/libpng16.zip",
+            checksum: "d811dfff741c2db81dee6b90ed93b4e02cff2d6037aefbd0df335078a0e93f62"
+        )
     ]
-
 )
+
