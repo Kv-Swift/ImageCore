e84c775383882dcca82e25d3f3b702368e3032ed
kivyswiftlink <kivyswiftlink@gmail.com>
2024-12-22 01:07:48 +0000

    Update from https://github.com/kivyswiftlink/CoreBuilder/commit/6ac70b219087f126103c6c54d23fccc7c31b7d85

--- Package.swift
+++ Package.swift
@@ -13,13 +13,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "libjpeg",
-            url: "https://github.com/kivyswiftlink/ImageCore/releases/download/311.0.14/libjpeg.zip",
-            checksum: "afcce020d4a418b63c855e955e57ecc690f29380e774d41b43826c8bae3c474b"
+            url: "https://github.com/kivyswiftlink/ImageCore/releases/download/311.0.15/libjpeg.zip",
+            checksum: "f1ffccc4737ec96c3b679919788cc4e0326af82da6f9b648b31ec32c85a185cf"
         ),
         .binaryTarget(
             name: "libpng16",
-            url: "https://github.com/kivyswiftlink/ImageCore/releases/download/311.0.14/libpng16.zip",
-            checksum: "9c65ef38b9cae9b4560833df65da4dd90d0f07681d3ff09a7b6f07a3449898d7"
+            url: "https://github.com/kivyswiftlink/ImageCore/releases/download/311.0.15/libpng16.zip",
+            checksum: "0b7e1bc74ed10636f5a1f8acd84810112f6775b740aee6d596e1ab153953a42d"
         )
     ]
 )
